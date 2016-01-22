7c933190a8300c62cf65c57866fd04db927fe593
Moustakime KIFIA <mkifia@icloud.com>
2016-01-22 11:52:43 +0100

    intro with scalar

---
+++ scalar.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $prefixe = "Au commencement,";
+my $suffixe = "a une fin";
+
+print "${prefixe} toute chose $suffixe\n";
+
+my $x;
+my $y = 10;
+my $z = "hello";
+
+$x = $y + 3;
+my $prenom = "Jules";
+my $phrase = "Bonjour $prenom";
+print $phrase."\n";
